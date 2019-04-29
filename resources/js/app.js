4b3bb82472a1f7750c793f3bef6cf2290dd8f92c
Michael Holod <git_ara@ukr.net>
2019-04-29 22:41:03 +0300

    npm packages updated, mediaqueries for the smallest size

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,9 +13,13 @@ window.Vue = require('vue');
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
  * or customize the JavaScript scaffolding to fit your unique needs.
- */
+ * Importing ES Modules
+ * As part of the vue-loader 15 updates, if your code uses the CommonJS syntax
+ * for importing EcmaScript modules, you'll need to append .default, like so:
+ * require('./components/ExampleComponent.vue').default
+ **/
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 const app = new Vue({
     el: '#app'
