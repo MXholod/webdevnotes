7ba4f2aecfdc59e30d3dad92e765cbcfd40f1869
Michael Holod <git_ara@ukr.net>
2019-06-10 21:18:19 +0300

    favicon.ico added to the admin panel

--- resources/views/admin/layouts/styles.blade.php
+++ resources/views/admin/layouts/styles.blade.php
@@ -1,8 +1,11 @@
 <!-- Fonts -->
 <link rel="dns-prefetch" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-<!-- Favicon favicon.ico -->
-<!--<link rel="shortcut icon" type="image/x-icon" sizes="16x16" href="{{ asset('/admin/images/favicon.ico') }}">-->
+<!-- Favicons -->
+<link rel="shortcut icon" type="image/x-icon" href="{{ asset('images/favicons/favicon.ico') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/favicon180x180.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicons/favicon32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicons/favicon16x16.png') }}">
 <!-- Styles -->
 <link href="{{ asset('css/admin/app_admin.css') }}" rel="stylesheet">
 {{-- Blade directive, we may push some 'styles' from another template
