9f5461724ca752c1d56190757c514967ed43cc3e
Michael Holod <git_ara@ukr.net>
2019-09-22 19:37:21 +0300

    Display Name of the registered user om main and admin pages

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -27,10 +27,13 @@
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->login }} <span class="caret"></span>
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    @if(isset(Auth::user()->email_verified_at) && (string) Auth::user()->email_verified_at !== '')
+                                        <a href="{{ url('/cabinet') }}">В кабинет</a>
+                                    @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
