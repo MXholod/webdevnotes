ab54d72aa018a57fccf997c95a382b2456c2930b
Michael Holod <git_ara@ukr.net>
2019-10-15 00:41:41 +0300

    Migration and Model for static pages created

---
+++ app/Models/BlogwdStaticPage.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Webdev\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogwdStaticPage extends Model
+{
+    //Work with Table 'blogwd_static_pages'
+    protected $table = 'blogwd_static_pages';
+    
+    //Get only published using Scope
+    public function scopeIsPublished($query){
+        return $query->where('published',1);
+    }
+}
