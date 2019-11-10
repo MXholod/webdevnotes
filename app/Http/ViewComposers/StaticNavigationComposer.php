ab2c249144ecd0a569bd61cf876e9b9a836ce949
Michael Holod <git_ara@ukr.net>
2019-11-10 17:26:53 +0200

    Fix static pages menu. Add slash if it doesn't stand at the first place in the 'path' property of DB

--- app/Http/ViewComposers/StaticNavigationComposer.php
+++ app/Http/ViewComposers/StaticNavigationComposer.php
@@ -8,7 +8,13 @@ class StaticNavigationComposer{
     public function compose(View $view){
         //Use Model scope
         $only_published = \Webdev\Models\BlogwdStaticPage::isPublished()->get();
-        //   
+        // 
+        foreach($only_published as $stPage){
+            //IF slash doesn't stand at the first place concat it.
+            if(strpos($stPage->path,'/') === FALSE){
+                $stPage->path = "/".$stPage->path;
+            }
+        }
         $view->with('staticPages',$only_published);
     }
 }
