c84b317ef6d49dedd5789014aed760725312ec6d
Michael Holod <git_ara@ukr.net>
2019-11-14 19:52:01 +0200

    Base controller is added for Admin Panel

---
+++ app/Http/Controllers/Admin/WDBlogBaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Webdev\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use Webdev\Http\Controllers\Controller;
+
+class WDBlogBaseController extends Controller
+{
+    //
+    
+}
