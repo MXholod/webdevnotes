68254f2bce98f775b62a88b73627bb5b47f1c792
Michael Holod <git_ara@ukr.net>
2019-12-14 18:06:45 +0200

    Model BlogwdScript.php created to store JS file paths

---
+++ app/Models/BlogwdScript.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Webdev\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BlogwdScript extends Model
+{
+    protected $fillable = [
+        'path_js',
+        'header_or_footer',
+        'scriptable_id',
+        'scriptable_type'
+    ];
+    //
+    public function scriptable()
+    {
+        return $this->morphTo();
+    }
+}
