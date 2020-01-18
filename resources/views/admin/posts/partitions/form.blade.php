811a606b38a66765c16c3ca2e56f89557259cf43
Michael Holod <git_ara@ukr.net>
2020-01-18 17:05:52 +0200

    Add newly created Vue component into the view form.blade.php

--- resources/views/admin/posts/partitions/form.blade.php
+++ resources/views/admin/posts/partitions/form.blade.php
@@ -33,12 +33,20 @@
 <label for="metaKeywords">meta-keywords</label>
 <input type="text" class="form-control" id="metaKeywords" name="meta_keywords" placeholder="meta-keywords" 
     value="{{$post->meta_keywords ?? ''}}" required />
-
+@if(isset($firstFiles) && isset($firstScripts))
+<create-aditional-js-css-component 
+    :file-paths="{{json_encode($firstFiles)}}" 
+    :db-paths="{{json_encode($firstScripts)}}"
+    >
+</create-aditional-js-css-component>
+@endif
+@if(isset($files) && isset($activeScripts))
 <aditional-js-css-component 
     :file-paths="{{json_encode($files)}}" 
     :db-paths="{{json_encode($activeScripts)}}"
     >
 </aditional-js-css-component>
+@endif
 <label for="description">Краткое описание статьи</label>
 <textarea cols="20" rows="2" id="description" name="description" style="width:100%">
     {{$post->description ?? ''}}
