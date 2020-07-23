0ddb0e0b92d947472fc29da18ca3152aa5b6a57c
Michael Holod <git_ara@ukr.net>
2020-07-24 00:07:35 +0300

    Package 'browser-sync' has been installed

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,5 +16,8 @@ mix.js('resources/js/app.js', 'public/js')
    //.js('resources/js/additional_js/article1.js', 'public/js/additional_js')
    .js(['resources/js/app.js','resources/js/admin/app_admin.js'], 'public/js/admin/app_admin.js')
    .sass('resources/sass/app.scss', 'public/css')
-   .sass('resources/sass/admin/app_admin.scss', 'public/css/admin')
+   .sass('resources/sass/admin/app_admin.scss', 'public/css/admin');
    //.sass('resources/sass/additional_css/test1.scss', 'public/css/additional_css')
+mix.browserSync({
+    proxy: 'http://webdevnotes'
+});
