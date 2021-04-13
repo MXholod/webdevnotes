e04f9b55e3f49aff9912dfd93d1316ef26fba709
Michael Holod <git_ara@ukr.net>
2021-04-13 03:13:11 +0300

    New JS paths were added to the 'webpack.mix.js' file they lead to the main JS on the client side and main JS on the admin side

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,10 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app_main_site.js', 'public/js')
+    .js('resources/js/app_main_admin.js', 'public/js')
     .js('resources/js/site/menu_left/menu_left.js','public/js/site/app_site.js')
    //.js('resources/js/additional_js/article1.js', 'public/js/additional_js')
-   .js(['resources/js/app.js','resources/js/admin/app_admin.js'], 'public/js/admin/app_admin.js')
+   .js(['resources/js/app_main_site.js','resources/js/admin/app_admin.js'], 'public/js/admin/app_admin.js')
    .sass('resources/sass/app.scss', 'public/css')
    .sass('resources/sass/admin/app_admin.scss', 'public/css/admin');
    //.sass('resources/sass/additional_css/test1.scss', 'public/css/additional_css')
